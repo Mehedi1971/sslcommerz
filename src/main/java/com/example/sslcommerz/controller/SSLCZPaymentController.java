b97f1a2747268e6eba0c3a026c6a3ea4d74bfdf7
shihab <mh.shihab001@gmail.com>
2023-12-08 17:44:26 +0600

    initialized payment gateway

--- src/main/java/com/example/sslcommerz/controller/SSLCZPaymentController.java
+++ src/main/java/com/example/sslcommerz/controller/SSLCZPaymentController.java
@@ -1,7 +1,8 @@
 package com.example.sslcommerz.controller;
 
-import com.example.sslcommerz.service.SSLCZPaymentService;
+import com.example.sslcommerz.service.TransactionInitiator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 public class SSLCZPaymentController {
 
-  private final SSLCZPaymentService sslczPaymentService;
+//  private final SSLCZPaymentService sslczPaymentService;
 
   @PostMapping("/initiate-payment")
   public String initiatePayment() {
     return null;
+  }
+
+  @GetMapping("/initiate")
+  public String initiatePayment1() {
+    TransactionInitiator transactionInitiator = new TransactionInitiator();
+
+    // Call the initTrnxnRequest method to initiate the transaction
+    String response = transactionInitiator.initTrnxnRequest();
+
+    // You can now send the response back to the client or process it further
+    // For example, you might redirect the client to the SSL Commerz payment page
+    // or return the response as JSON to the client
+    return response;
+  }
+
+  @GetMapping("/success")
+  public String successMessage(){
+    return "Success";
   }
 }
